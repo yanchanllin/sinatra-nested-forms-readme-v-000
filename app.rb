310e786a3ffe748fab1e5506ab2c27465bf5a1e2
YanC Lin <yonalin1@gmail.com>
2018-03-13 21:05:57 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,5 +1,23 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
-
+  get '/' do
+       erb :root
+     end
+ 
+     get '/new' do
+       erb :'pirates/new'
+     end
+ 
+     post '/pirates' do
+       params["pirate"]["ships"].each do |ship_data|
+         Ship.new(ship_data["name"], ship_data["type"], ship_data["booty"])
+       end
+ 
+       @pirate = Pirate.new(params["pirate"]["name"], params["pirate"]["height"], params["pirate"]["weight"])
+ 
+       @ships = Ship.all
+ 
+       erb :'pirates/show'
+     end
 end
