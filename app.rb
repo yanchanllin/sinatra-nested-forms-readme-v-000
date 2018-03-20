e968782cb6655d2e39a07444ddd77edaafd607cf
YanC Lin <yonalin1@gmail.com>
2018-03-20 14:22:20 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,23 +1,20 @@
-require_relative 'config/environment'
+require './environment'
 
-class App < Sinatra::Base
-  get '/' do
-       erb :root
-     end
- 
-     get '/new' do
-       erb :'pirates/new'
-     end
- 
-     post '/pirates' do
-       params["pirate"]["ships"].each do |ship_data|
-         Ship.new(ship_data["name"], ship_data["type"], ship_data["booty"])
-       end
- 
-       @pirate = Pirate.new(params["pirate"]["name"], params["pirate"]["height"], params["pirate"]["weight"])
- 
-       @ships = Ship.all
- 
-       erb :'pirates/show'
-     end
+module
+  class App < Sinatra::Base
+
+get '/' do
+  erb :new
+end
+
+post '/student' do
+  @student = Student.new(params[:student])
+
+  params[:student][:courses].each do |details|
+    Course.new(details)
+  end
+
+  @courses = Course.all
+
+  erb :student
 end
